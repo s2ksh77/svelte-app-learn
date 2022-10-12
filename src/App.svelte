ce9e90813ea2f3e0c91f46b1f4c8889b105c60e9
soohyun <soohyun_kim2@tmax.co.kr>
2022-10-12 16:55:57 +0900

    선언적 렌더링

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,46 @@
 <script>
-	export let name;
+  export let name;
+  export let age = 85;
+  const assign = () => {
+    name = 'Heorpy';
+    age = 36;
+  };
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Hello {name}!</h1>
+  <h2 class={age < 85 ? 'active' : ''}>{age}</h2>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build
+    Svelte apps.
+  </p>
+  <img src="" alt={name} />
+  <input type="text" bind:value={name} />
+  <button on:click={assign}> Assign </button>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  .active {
+    color: blue;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
